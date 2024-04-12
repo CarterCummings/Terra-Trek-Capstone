9523b1e5ae2f3d0f9f7fe8af42161bb6f6c19e5e
CarterCummings <cartercummings1@gmail.com>
2024-04-12 15:30:40 -0500

    created motor function templates

---
+++ Test Bench/Motors/L1_Motor.py
@@ -0,0 +1,29 @@
+# Name: L1_Motor
+# Date: 4 / 12 / 2024
+# 
+# Author: Carter Cummings
+# Purpose: Run diffrent motor types
+
+
+#
+def driveMotor(power,direction):
+    # Imports  
+    print('driving at: ' + str(power))
+
+
+#
+def spin(power,direction):
+    #Imports
+    print('spinning at: ' + str(power))
+
+
+#
+def driveStepper(power,direction):
+    #imports
+    print('driving stepper at: ' + str(power))
+
+
+
+driveMotor(1,1)
+spin(1,1)
+driveStepper(1,1)
