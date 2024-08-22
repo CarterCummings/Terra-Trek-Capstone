4f5fea547f79b5706a957e043a8c971e9b966f1a
cartercummings <cartercummings1@gmail.com>
2024-08-22 14:39:18 -0500

    slam added params need tuning

--- src/terratrek_robot/launch/launch_sim.launch.py
+++ src/terratrek_robot/launch/launch_sim.launch.py
@@ -5,6 +5,7 @@ from ament_index_python.packages import get_package_share_directory
 from launch import LaunchDescription
 from launch.actions import IncludeLaunchDescription
 from launch.launch_description_sources import PythonLaunchDescriptionSource
+from launch.substitutions import LaunchConfiguration
 
 
 from launch_ros.actions import Node
@@ -15,6 +16,7 @@ def generate_launch_description():
     # Include the robot_state_publisher launch file, provided by our own package. Force sim time to be enabled
     # !!! MAKE SURE YOU SET THE PACKAGE NAME CORRECTLY !!!
  
+    use_sim_time = LaunchConfiguration('use_sim_time', default='false') 
 
     package_name='terratrek_robot' #<--- CHANGE ME
 
@@ -24,11 +26,21 @@ def generate_launch_description():
                 )]), launch_arguments={'use_sim_time': 'true'}.items()
     )
 
+    world = os.path.join(get_package_share_directory('terratrek_robot'),'mainworld.world')
 
     # Include the Gazebo launch file, provided by the gazebo_ros package
+    #gazebo = IncludeLaunchDescription(
+    #            PythonLaunchDescriptionSource([os.path.join(
+    #                get_package_share_directory('gazebo_ros'), 'launch', 'gazebo.launch.py'
+    #            )]), launch_arguments={'world': "$(find terratrek_robot)/worlds/mainworld.world"}.items()
+    #)
+
+    gazebo_params_file = os.path.join(get_package_share_directory(package_name),'config','gazebo_params.yaml')
+
     gazebo = IncludeLaunchDescription(
                 PythonLaunchDescriptionSource([os.path.join(
                     get_package_share_directory('gazebo_ros'), 'launch', 'gazebo.launch.py')]),
+                    launch_arguments={'extra_gazebo_args': '--ros-args --params-file ' + gazebo_params_file}.items()
              )
 
     # Run the spawner node from the gazebo_ros package. The entity name doesn't really matter if you only have a single robot.
@@ -62,6 +74,13 @@ def generate_launch_description():
 #        arguments=["pos_cont_left"],
 #    )
 
+    static_transform = Node(
+            package='tf2_ros',
+            namespace = 'scan_to_map',
+            executable='static_transform_publisher',
+            arguments= ["0", "0", "0", "0", "0", "0", "map", "scan"]
+        )
+
     joystick = IncludeLaunchDescription(
                 PythonLaunchDescriptionSource([os.path.join(
                     get_package_share_directory(package_name),'launch','joystick.launch.py'
@@ -77,5 +96,6 @@ def generate_launch_description():
         skid_drive_spawner,
         joint_broad_spawner,
         joystick,
+        static_transform,
         #position_cont_spawner,
     ])
